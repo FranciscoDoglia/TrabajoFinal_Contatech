2b71770cc990ce464e3d5c7877ccf934840a6637
FranciscoDoglia <103296461+FranciscoDoglia@users.noreply.github.com>
2022-06-26 17:28:56 -0300

    Update db.py

--- db.py
+++ db.py
@@ -1,18 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
 
-
-engine = create_engine('postgresql://lknvayullpsvnj:d0c1fd3a599e7797a276a6e8b51d6e5215a41009cfc8b19b21aa2f26c2ca3c36@ec2-52-71-23-11.compute-1.amazonaws.com:5432/dbi1756bb2od2k')
+engine = create_engine('postgresql://lknvayullpsvnj:d0c1fd3a599e7797a276a6e8b51d6e5215a41009cfc8b19b21aa2f26c2ca3c36@ec2-52-71-23-11.compute-1.amazonaws.com:5432/dbi1756bb2od2k', pool_pre_ping=True)
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
+# Base.query = session.query_property()
 
-
-
-def run():
-    pass
-#
-# if __name__ == '__main__':
-#     Base.metadata.create_all(engine)
-#     run()
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
