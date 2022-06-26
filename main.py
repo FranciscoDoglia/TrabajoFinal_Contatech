de49235d70ea0bffb344b5f98ae9f5d260b0618a
FranciscoDoglia <103296461+FranciscoDoglia@users.noreply.github.com>
2022-06-26 17:30:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from flask import Flask, request, json, jsonify
-import requests
-
 import db
-# from cliente_prueba import Cliente
-import model
 from model import Cliente
 
 app = Flask(__name__)
@@ -12,25 +8,22 @@ app = Flask(__name__)
 def run():
     pass
 
-@app.route('/add_client', methods=['GET', 'POST'])
+@app.route('/add_client', methods=['POST'])
 def datos_cte():
-    # nombre = request.args['nombre']
-    # documento = request.args['documento']
     request_json = request.json
-    # print(request_json['nombre'],request_json['documento'])
-    new_client = Cliente(request_json['id'], request_json['nombre'])
+    nombre = request_json['nombre']
+    documento = request_json['documento']
+    new_client = Cliente(nombre, documento)
     db.session.add(new_client)
     db.session.commit()
-    return f'{new_client.id}Hello {new_client.nombre}'
+    return f'{new_client.nombre}Hello {new_client.documento}, {new_client.id}'
     #PARA PROBAR USAR API https://inspector.swagger.io/builder
 
 @app.route('/consulta')
 def consulta():
-    myCliente = Cliente.query.all()
+    myCliente = db.session.query(Cliente).all() #Cliente.query.all()
     return jsonify(myCliente)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
-    db.Base.metadata.create_all(db.engine)
     run()
-
