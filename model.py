1a8aaf328a3b7cd1a9ae46dbc9764efb60a669a4
FranciscoDoglia <103296461+FranciscoDoglia@users.noreply.github.com>
2022-07-03 14:58:23 -0300

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import relationship
 import db
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey, Float
 
 
 class Cliente(db.Base):
@@ -27,10 +27,40 @@ class Factura(db.Base): #Podría guardarlas en la base de datos también
     __tablename__ = 'factura'
     id = Column(Integer, primary_key=True)
     numero = Column(String, nullable=False)
+    neto_gravado = Column(Float, nullable=False)
+    alic_iva = Column(Float, nullable=True)
     cliente_id = Column(Integer, ForeignKey('cliente.id'))
 #Agregar que la columna numero no puede repetirse para el mismo cliente_id
 
-    def __init__(self, numero: str):
+    def __init__(self, numero: str, neto_gravado: float, alic_iva: float):
         super(Factura, self).__init__()
         self.numero = numero
+        self.neto_gravado = neto_gravado
+        self.alic_iva = alic_iva
+
+    def __devolver_total(self) -> float:
+        total = self.neto_gravado * (1 + (self.alic_iva/100))
+        return total
+
+    @property
+    def total(self):
+        return self.__devolver_total()
+
+    def __devolver_iva(self) -> float:
+        iva = self.neto_gravado *  (self.alic_iva / 100)
+        return iva
+
+    @property
+    def iva(self):
+        return self.__devolver_iva()
+
+
+    # def print_fc(self):     # A partir de tomar una plantilla modelo de factura
+    #     print(self.parent.nombre)
+    #     print(self.parent.documento)
+    #     print(self.total - self.iva)
+    #     print(self.iva)
+    #     print(self.total)
+    #     pass
+
 
